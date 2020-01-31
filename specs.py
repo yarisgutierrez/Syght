3610685687d179cfb88860b7955304c97d70f8d4
mccomb <markm@bigid.com>
2020-01-31 06:24:56 +0000

    Mcombs lame changes

--- specs.py
+++ specs.py
@@ -5,6 +5,7 @@ import sys
 import json
 import logging
 import argparse
+import shutil
 
 from system_data import sys_info
 from docker_data import calc_cpu, calc_cpu2, calc_blkio_bytes,\
@@ -12,13 +13,20 @@ from docker_data import calc_cpu, calc_cpu2, calc_blkio_bytes,\
 
 logger = logging.getLogger(__name__)
 
-
 def env_var():
     for k, v in os.environ.items():
         yield(f'{k}: {v}')
 
 
 def main():
+    tmp_output_dir = "./Syght_output"
+    output_zip_filename = "./Syght_output.zip"
+    try:
+        os.mkdir(tmp_output_dir)
+    except OSError:
+        # Don't worry about the error if the dir exists. We can always use the existing data! 
+        print ("")
+
     parser = argparse.ArgumentParser(
             description="BigID System Data Aggregator"
             )
@@ -61,39 +69,48 @@ def main():
             'environment_variables': list(env_var())
         }
         # Export Environment Information
-        with open('environment.json', 'w', encoding='utf-8') as f:
+        with open(tmp_output_dir + '/environment.json', 'w', encoding='utf-8') as f:
             json.dump(env_data, f, ensure_ascii=False, indent=4)
 
     if args.datasource:
         from bigid_data import config
         # Export Data Sources
-        with open('ds_configuration.json', 'w', encoding='utf-8') as f:
+        with open(tmp_output_dir + '/ds_configuration.json', 'w', encoding='utf-8') as f:
             json.dump(config("ds_connections"), f, ensure_ascii=False,
                       indent=4)
 
     if args.entitysource:
         from bigid_data import config
         # Export Entity Sources
-        with open('es_configuration.json', 'w', encoding='utf-8') as f:
+        with open(tmp_output_dir + '/es_configuration.json', 'w', encoding='utf-8') as f:
             json.dump(config("id_connections"), f, ensure_ascii=False,\
                       indent=4)
 
     if args.dsar:
         from bigid_data import config
         # Export DSAR configuration
-        with open('sar_configuration.json', 'w', encoding='utf-8') as f:
+        with open(tmp_output_dir + '/sar_configuration.json', 'w', encoding='utf-8') as f:
             json.dump(config("sar/config"), f, ensure_ascii=False,\
                        indent=4)
 
     if args.containers:
         # Export Container Information
-        with open('containers.json', 'w', encoding='utf-8') as f:
+        with open(tmp_output_dir + '/containers.json', 'w', encoding='utf-8') as f:
             json.dump(list(container_stats()), f, ensure_ascii=False, indent=4)
 
     if args.logs:
         from bigid_data import bigid_logs
         # Export Services logs
         bigid_logs()
+
+    shutil.make_archive("Syght_output", 'zip', tmp_output_dir)
+
+    # rm the the tmp output dir
+    try:
+        shutil.rmtree(tmp_output_dir)
+    except OSError:
+        # WHo cares?
+        print ("")
 
 
 if __name__ == "__main__":
